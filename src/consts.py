56faf57306fb161aeec854c41cf1c81137f99f6c
Abdo <abd.nh25@gmail.com>
2022-05-02 09:26:53 +0300

    Add template filter and do some refactoring to separate files

--- src/consts.py
+++ src/consts.py
@@ -4,3 +4,4 @@ ADDON_NAME = "Copy Around"
 ADDON_DIR = os.path.dirname(__file__)
 ADDON_PACKAGE = os.path.basename(ADDON_DIR)
 ICONS_DIR = os.path.join(ADDON_DIR, "icons")
+FILTER_NAME = "copyaround"
---
+++ src/filter.py
@@ -0,0 +1,54 @@
+import re
+from anki.hooks import field_filter
+from anki.template import TemplateRenderContext
+from aqt import mw
+
+from . import consts
+from .copy_around import get_related_content
+
+# FIXME: doesn't work with values that contain double quotes
+FILTER_OPTION_RE = re.compile(r'((?P<key>\w+)\s*=\s*(?P<value>(".*")|\S*))')
+
+
+def add_filter(
+    field_text: str,
+    field_name: str,
+    filter_name: str,
+    ctx: TemplateRenderContext,
+) -> str:
+    """
+    Adds a "copyaround" template filter. E.g.
+    ```
+    {{copyaround deck=leech_deck_2 search_in=Expression leech_from=Snapshot shuffle=true:word}}
+    ```
+    """
+    if not filter_name.startswith(consts.FILTER_NAME):
+        return field_text
+    options = {}
+    options_text = filter_name.split(maxsplit=1)[1]
+    for match in FILTER_OPTION_RE.finditer(options_text):
+        pair = match.groupdict()
+        key = pair["key"]
+        value = pair["value"]
+        options[key] = value
+
+    did = mw.col.decks.id(options["deck"])
+    shuffle = (
+        (True if options["random"].lower() == "true" else False)
+        if "random" in options
+        else True
+    )
+    ret = get_related_content(
+        ctx.note(),
+        did,
+        field_name,
+        options.get("search_in", ""),
+        options["leech_from"],
+        1,
+        shuffle=shuffle,
+    )
+    return ret
+
+
+def init_filter():
+    field_filter.append(add_filter)
